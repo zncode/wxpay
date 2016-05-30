d22bda1a31819e34660969617d1d13be0993f060
zhaoning@openfin.com <znonline>
2016-05-30 14:45:02 +0800

    up

--- lib/WxPay.Config.php
+++ lib/WxPay.Config.php
@@ -22,8 +22,8 @@ class WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx426b3015555a46be';
-	const MCHID = '1225312702';
+	const APPID = 'wx8671ac3b61e9d2ed';
+	const MCHID = '1349336901';
 	const KEY = 'e10adc3949ba59abbe56e057f20f883e';
 	const APPSECRET = '01c6d59a3f9024db6336662ac95c8e74';
 	
